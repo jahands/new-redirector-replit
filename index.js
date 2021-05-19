223eb80d46867dd3974d47d847c16dff7fd39ce6
Jacob Hands <jacob@gogit.io>
2021-05-19 21:05:34 +0000

    Validate languages and allow only specifying name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 
@@ -41,6 +42,11 @@ const langMap = {
     'web': 'html' // A bit generic but I think it's a good mapping
 };
 
+let language_keys_cache = {
+    timestamp: 0,
+    language_keys: []
+}
+
 // For static html, etc
 app.use('/static', express.static('public'))
 // Help page. WIP
@@ -48,15 +54,65 @@ app.get('/help', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'help.html'));
 });
 
-app.get('/:lang?/:name?', (req, res) => {
+app.get('/:lang?/:name?', async (req, res) => {
     let lang = req.params.lang || 'bash';
     if (langMap.hasOwnProperty(lang)) {
         lang = langMap[lang];
     }
-    const name = req.params.name ? `?name=${req.params.name}` : ''
-    res.redirect(`https://replit.com/new/${lang}${name}`);
+    let name = req.params.name || ''
+    const isValidLang = await isValidLanguageKey(lang)
+    if (!isValidLang) {
+        if (name === '') {
+            // Assume they only specified the name
+            name = lang
+            lang = 'bash'
+        } else {
+            // They specified a name but an invalid language
+            // default to bash
+            lang = 'bash'
+        }
+    }
+    let nameParam = name !== '' ? `?name=${name}` : ''
+    res.redirect(`https://replit.com/new/${lang}${nameParam}`);
 });
 
 app.listen(3000, () => {
     console.log('server started');
 });
+
+
+async function getLanguageKeys() {
+    now = Date.now()
+    if (now - language_keys_cache.timestamp < 86400) {
+        return language_keys_cache.language_keys
+    }
+    try {
+        // Update from upstream
+        const url = 'https://replit-language-api.uuid.rocks/api/languages/keys';
+        const res = await fetch(url)
+        const json = await res.json()
+        const keys = json.language_keys
+        language_keys_cache.language_keys = keys
+        language_keys_cache.timestamp = Date.now()
+        return keys
+    } catch (e) {
+        // Use cached if we fail to get upstream
+        return language_keys_cache.language_keys
+    }
+}
+async function isValidLanguageKey(name) {
+    try {
+        if (langMap.hasOwnProperty(name)) {
+            return true
+        }
+        const keys = await getLanguageKeys()
+        if (keys.length === 0) {
+            return true // In case something bad happens
+        }
+        const valid = keys.indexOf(name) > -1
+        return valid
+    } catch (e) {
+        // Default to valid if anything goes wrong
+        return true
+    }
+}
