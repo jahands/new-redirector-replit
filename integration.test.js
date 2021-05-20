3d9d0b204ea73d9c5ccd9626d415f90286baa227
Jacob Hands <jacob@gogit.io>
2021-05-20 02:21:38 +0000

    Test for help page

--- integration.test.js
+++ integration.test.js
@@ -4,12 +4,14 @@ const fetch = require('node-fetch');
 // to ensure that the correct responses are being given.
 // run with `jest` or `npm run test`
 
+const baseUrl = 'https://new-redirector.amasad.repl.co'
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+const headers = {
+    'User-Agent': userAgent
+};
+
 async function match(req_path) {
-    const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
-    const headers = {
-        'User-Agent': userAgent
-    };
-    const url = 'https://new-redirector.amasad.repl.co' + req_path;
+    const url = baseUrl + req_path;
     const res = await fetch(url, { headers: headers, redirect: 'manual' });
     return res.headers.get('Location')
         .replace('https://replit.com', '');
@@ -48,5 +50,14 @@ test('Language (shortcut):\n\t/py -> /new/python3', () => {
 test('Language (shortcut) + name:\n\t/py/some_name -> /new/python3?name=some_name', () => {
     return match('/py/some_name').then(path => {
         expect(path).toBe('/new/python3?name=some_name');
+    });
+});
+
+test('Make sure help page appears to exist', () => {
+    return fetch(baseUrl + '/help', { headers: headers }).then(res => {
+        expect(res.status).toBe(200);
+        return res.text().then(text => {
+            expect(text.indexOf('<!doctype html>')).toBe(0)
+        });
     });
 });
